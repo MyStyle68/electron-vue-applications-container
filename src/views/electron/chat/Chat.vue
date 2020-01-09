56c201e8e2c972fa90d461e7b897e06c98b686aa
wuhen <15210204903@163.com>
2020-01-09 15:06:04 +0800

    聊天写到一半，进行工程拆分

--- src/views/electron/chat/Chat.vue
+++ src/views/electron/chat/Chat.vue
@@ -1,33 +1,11 @@
 <template>
   <div class="chat-view">
-    <div @mousedown="mousedown($event)" class="chat-top">
-      <span @click="hideWindow" title="关闭" class="close-btn icon icon-close"></span>
-      <div class="user-info">
-        <div class="left">
-          <div class="user-img">
-            <Dropdown class="state-pos">
-              <div :class="`now-state ${imgState}`"></div>
-              <DropdownMenu slot="list">
-                <DropdownItem>
-                  <RadioGroup v-model="imgState" vertical>
-                    <Radio label="online">
-                      <i class="online"></i>
-                      <span>在线</span>
-                    </Radio>
-                    <Radio label="busy">
-                      <i class="busy"></i>
-                      <span>忙碌</span>
-                    </Radio>
-                  </RadioGroup>
-                </DropdownItem>
-              </DropdownMenu>
-            </Dropdown>
-          </div>
-        </div>
-        <div class="right">
-
-        </div>
-      </div>
+    <TopContent/>
+    <Selects/>
+    <div class="msg-content">
+      <Vuescroll :ops="ops">
+        <router-view/>
+      </Vuescroll>
     </div>
   </div>
 </template>
@@ -36,53 +14,36 @@
 
   import {Component, Vue} from "vue-property-decorator";
 
-  import {Dropdown, DropdownMenu, DropdownItem, RadioGroup, Radio, Icon} from "view-design";
+  import TopContent from "@/views/electron/chat/top/TopContent.vue";
+  import Selects from "@/views/electron/chat/selects/Selects.vue";
+  import PeopleGroup from "@/views/electron/chat/message/people/PeopleGroup.vue";
+
+  import Vuescroll from "vuescroll"
 
   @Component({
     components: {
-      Dropdown, DropdownMenu, DropdownItem, RadioGroup, Radio, Icon
+      PeopleGroup,
+      TopContent, Selects, Vuescroll
     }
   })
   export default class Chat extends Vue {
-
-    private nowState: 'online' | 'busy' = "online";
-
-    private get imgState() {
-      return this.nowState;
-    }
-
-    private set imgState(str: 'online' | 'busy') {
-      console.log("除法")
-      this.nowState = str
-    }
-
-    private hideWindow() {
-      this.electronRemote.getCurrentWindow().hide()
-    }
-
-    private mousedown(e: any) {
-      const remote = this.electronRemote;
-      const currWindowPosition = remote.getCurrentWindow().getPosition();
-      let formX = e.screenX - currWindowPosition[0];
-      let formY = e.screenY - currWindowPosition[1];
-      document.onmousemove = function (e) {
-        remote.getCurrentWindow().setPosition((e.screenX - formX), (e.screenY - formY));
-      };
-      document.onmouseup = () => {
-        document.onmousemove = null;
-        document.onmouseup = null;
+    private ops = {
+      scrollPanel: {
+        scrollingX: false
+      },
+      bar: {
+        background: '#888888'
       }
     }
+
   }
 </script>
 
 <style lang="less" scoped>
-  .online {
-    background-color: lawngreen;
-  }
+  @import "../../../assets/fonts/chat/chatFont.css";
 
-  .busy{
-    background-color: darkorange;
+  * {
+    user-select: none;
   }
 
   .chat-view {
@@ -96,73 +57,16 @@
     overflow: hidden;
     margin: 0;
     padding: 0;
+    background-color: rgba(0, 0, 0, 0.3);
 
-    .chat-top {
-      width: 100%;
-      height: 110px;
-      background-color: red;
-      cursor: move;
-
-      .close-btn:hover {
-        color: rgba(0, 0, 0, 0.6);
-      }
-
-      .close-btn {
-        z-index: 9999999999;
-        color: rgba(0, 0, 0, 0.9);
-        padding: 3px;
-        line-height: 18px;
-        font-size: 18px;
-        cursor: pointer;
-        position: absolute;
-        right: 12px;
-        top: 12px;
-        text-align: center;
-        /*background-color: rgba(0,0,0,0.6);*/
-      }
-
-      .user-info {
-        width: 100%;
-        height: 100%;
-        position: relative;
-        background-color: transparent;
-
-        .left {
-          width: 110px;
-
-          .user-img {
-            cursor: pointer;
-            position: relative;
-            width: 58px;
-            height: 58px;
-            background-image: url("../../../assets/timg.jpeg");
-            background-size: 100% 100%;
-            border-radius: 29px;
-            margin: 18px 0 0 12px;
-
-            .state-pos {
-              position: absolute;
-              right: 5px;
-              bottom: 0;
-            }
-
-            .now-state {
-              width: 12px;
-              height: 12px;
-              border-radius: 6px;
-              position: absolute;
-              bottom: 0;
-              right: 0;
-            }
-          }
-        }
-
-        .left, .right {
-          float: left;
-          height: 100%;
-        }
-      }
-
+    .msg-content {
+      position: absolute;
+      left: 0;
+      right: 0;
+      top: 146px;
+      bottom: 0;
+      overflow: hidden;
+      background-color: rgba(255, 255, 255, 0.6);
     }
   }
 </style>
---
+++ src/views/electron/chat/message/people/PeopleItem.vue
@@ -0,0 +1,88 @@
+<template>
+  <div @click.stop.prevent="" class="people-item">
+    <div class="left">
+      <img src="../../../../../assets/timg.jpeg" alt="头像">
+    </div>
+    <div class="right">
+      <span class="user-name">wuhen(大帅锅)</span>
+      <span class="user-sign">没办法太帅了!没办法太帅了!没办法太帅了!没办法太帅了!没办法太帅了!没办法太帅了!</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import {Component, Vue} from "vue-property-decorator";
+
+  @Component({})
+  export default class PeopleItem extends Vue {
+
+  }
+</script>
+
+<style lang="less" scoped>
+  .people-item {
+    width: 100%;
+    height: 52px;
+    margin: 8px 0;
+    padding: 5px 15px 5px 15px;
+    position: relative;
+    vertical-align: middle;
+
+    &:hover {
+      background-color: rgba(0, 0, 0, .05);
+      cursor: pointer;
+    }
+
+    & > div {
+      float: left;
+
+      span {
+        width: 100%;
+
+      }
+
+      .user-name {
+        color: #333333;
+        font-weight: bold;
+        font-size: 14px;
+      }
+
+      .user-sign {
+        width: 98%;
+        color: #999999;
+        font-size: 12px;
+        display: block;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+      }
+    }
+
+    .right {
+      padding-top: 8px;
+      padding-left: 8px;
+      position: absolute;
+      right: 0;
+      left: 52px;
+      top: 0;
+      bottom: 0;
+    }
+
+    .left {
+      padding-top: 8px;
+      position: absolute;
+      left: 0;
+      top: 0;
+      bottom: 0;
+      width: 52px;
+      text-align: right;
+      line-height: 52px;
+
+      img {
+        width: 36px;
+        height: 36px;
+        border-radius: 100%;
+      }
+    }
+  }
+</style>
